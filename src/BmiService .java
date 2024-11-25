57bb8c8210c871f310657cab71b4472ba4b93641
Zaki31607 <kakashvili.zurab@mail.ru>
2024-11-25 17:05:24 +0500

    Update BmiService .java

--- src/BmiService .java
+++ src/BmiService .java
@@ -5,17 +5,3 @@ public class BmiService {
         return (int) bmi;
     }
 }
-
-public class Main {
-
-    public static void main(String[] args) {
-        BmiService bmiService = new BmiService();
-
-        double heightInMeters = 1.87;
-        double weightInKilograms = 98.0;
-
-        int bmi = bmiService.calculate(heightInMeters, weightInKilograms);
-
-        System.out.println("Ваш ИМТ: " + bmi);
-    }
-}
